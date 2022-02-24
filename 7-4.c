35675991d39a508d09c3d0fde55e04dcc1082f1d
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-25 01:51:43 +0900

    7-4

---
+++ 7-4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+  srand((unsigned)time(NULL)); //乱数発生の準備
+  enum
+  {
+    FIRE,
+    WATER,
+    WIND,
+    EARTH
+  }; //宝石の属性を示す定数(０〜３)
+  int gems[10];
+
+  //宝石の属性をランダム
+  for (int i = 0; i < 10; i++)
+  {
+    int gemType = rand() % 4;
+    gems[i] = gemType;
+  }
+  //
+  for (int i = 0; i < 10; i++)
+  {
+    switch (gems[i])
+    {
+    case FIRE:
+      printf("$");
+      break;
+    case WATER:
+      printf("*");
+      break;
+    case WIND:
+      printf("@");
+      break;
+    case EARTH:
+      printf("#");
+      break;
+    }
+    printf(" "); //見やすいようにスペース入れる
+  }
+  return 0;
+}
